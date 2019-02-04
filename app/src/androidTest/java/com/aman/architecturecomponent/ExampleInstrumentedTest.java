d87dd3e7c3afb6e57b89303c1c96d91f0c328421
Aman Bansal <aman210697@gmail.com>
2019-02-04 17:54:34 +0530

    espresso test

--- app/src/androidTest/java/com/aman/architecturecomponent/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/aman/architecturecomponent/ExampleInstrumentedTest.java
@@ -2,11 +2,22 @@ package com.aman.architecturecomponent;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -16,11 +27,37 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @Rule
+
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
+
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.aman.architecturecomponent", appContext.getPackageName());
+    }
+
+    @Test
+    public void uiTest(){
+
+
+
+        //click on the add note button
+        onView(withId(R.id.fab)).perform(click());
+
+        //add title and description
+        onView(withId(R.id.editText_title)).perform(typeText("test title"));
+
+        onView(withId(R.id.editText_description)).perform(typeText("test description"));
+
+        //save the note
+        onView(withId(R.id.save_note)).perform(click());
+
+        //verify note is displayed on screen
+        onView(withText("test description")).check((ViewAssertions.matches(isDisplayed())));
+
     }
 }
